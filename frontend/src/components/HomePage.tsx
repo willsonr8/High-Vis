f42bb29c81bcdfbd41bef78b3efc9776a964986a
willsonr8 <willsonr8@gmail.com>
2024-07-16 22:53:45 -0400

    Adds navbar and title

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -2,12 +2,24 @@
 function HomePage() {
     return (
         <div className={"all-container"}>
-            <div className={"bg-white items-center"}>
-                <div>
+            <header className={"title-container"}>
+                <div className={"home-header bg-black"}>
+                    <div className={"header-left"}>
+                        <h2 className={"title-text text-xl text-white font-bold"}>{"High-Vis"}</h2>
+                    </div>
+                    <div className={"header-right"}>
+                        <h2 className={"title-text text-xl text-white font-bold"}>{"Buttons"}</h2>
+                    </div>
                 </div>
-            <h2 className="text-xl text-black font-bold text-center mb-6">{"NFL DATA VISUALIZER"}</h2>
-            </div>
+                <div className={"navbar bg-black"}>
+                    <div className={"navbar-items"}>
+                        <a className={"navbar-text text-white"}>{"Line Item 1"}</a>
+                        <a className={"navbar-text text-white"}>{"Line Item 2"}</a>
+                    </div>
+                </div>
+            </header>
         </div>
-)}
+    )
+}
 
 export default HomePage
