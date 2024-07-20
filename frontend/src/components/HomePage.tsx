b52731f645db23f6a103fc11454345c3a9855f4f
willsonr8 <willsonr8@gmail.com>
2024-07-20 13:44:54 -0400

    Fixes search bar typing

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -9,7 +9,7 @@ function HomePage() {
                 </p>
             </div>
             <div className="mt-4 mb-4 flex items-center justify-between gap-2 md:mt-8 md:mb-8">
-                <Search placeholder="Search players..."/>
+                <Search/>
             </div>
         </div>
     )
--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -13,30 +13,25 @@ const Search = ({ onSearch }) => {
     onSearch(query);
   };
 
-// const Search({ placeholder }: { placeholder: string }) {
   function handleSearch(term: string) {
     console.log(term);
   }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
       <form className="search-form" onSubmit={handleSubmit}>
         <input
+            type={"text"}
             className="primary-search peer block border w-3/5 border-gray-200 text-sm outline-2 placeholder:text-gray-500"
             placeholder={"Search players"}
             value={query}
-            onChange={(e) => {
-              handleSearch(e.target.value);
-            }}
+            onChange={handleChange}
         />
         {/*<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-full text-gray-500 peer-focus:text-gray-900" />*/}
-        <button type="submit">Search</button>
+          <div className={"search-button-div"}><button className={"button-submit"} type="submit">Search</button></div>
       </form>
     </div>
   );
 }
 
-export default Search
+export default Search;
