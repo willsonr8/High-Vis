669cc1392431885c6520fb160632af9669835819
willsonr8 <willsonr8@gmail.com>
2024-08-11 12:09:28 -0400

    Adds all team logos including logic for variable logo display

--- frontend/src/components/player_name/PlayerPage.tsx
+++ frontend/src/components/player_name/PlayerPage.tsx
@@ -1,10 +1,45 @@
 import React from "react";
 
+const team_dict = {
+    "ARI": "cardinals",
+    "ATL": "falcons",
+    "BAL": "ravens",
+    "BUF": "bills",
+    "CAR": "panthers",
+    "CHI": "bears",
+    "CIN": "bengals",
+    "CLE": "browns",
+    "DAL": "cowboys",
+    "DEN": "broncos",
+    "DET": "lions",
+    "GB": "packers",
+    "HOU": "texans",
+    "IND": "colts",
+    "JAX": "jaguars",
+    "KC": "chiefs",
+    "LAC": "chargers",
+    "LAR": "rams",
+    "LV": "raiders",
+    "MIA": "dolphins",
+    "MIN": "vikings",
+    "NO": "saints",
+    "NYG": "giants",
+    "NYJ": "jets",
+    "PHI": "eagles",
+    "PIT": "steelers",
+    "SEA": "seahawks",
+    "SF": "49ers",
+    "TB": "buccaneers",
+    "TEN": "titans",
+    "WAS": "commanders"
+}
+
 const PlayerPage = ({player}) => {
-    console.log(player)
     if (!player) {
         return <p className={"text-white center"}>Loading player data...</p>;
     }
+    const logo = team_dict[player.team];
+    const id = player.espnID;
     return (
         <div>
             <div className={"container-1"}>
@@ -22,7 +57,7 @@ const PlayerPage = ({player}) => {
                         <span>{`${player.age} y/o, ${player.bDay}`}</span>
                     </div>
                     <div className={"team-image-container"}>
-                        <img src={"/team_logos/bears-logo.png"} alt="team logo"/>.
+                        <img src={`/team_logos/${logo}-logo.png`} alt="team logo"/>.
                     </div>
                 </div>
             </div>
